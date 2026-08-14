badcb0d0c876a51739037a6dac712cb03c85c020
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-15 00:05:45 +0300

    refactor(course): rename ecdtsCredits to credits

---
+++ src/main/java/com/heigraduate/app/graduate/dto/CourseRequest.java
@@ -0,0 +1,10 @@
+package com.heigraduate.app.graduate.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record CourseRequest(
+    @NotBlank String courseReference,
+    @NotBlank String title,
+    @NotNull @Positive Integer Credits) {}
