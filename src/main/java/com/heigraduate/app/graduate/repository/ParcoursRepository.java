7b158ba59fd9c43c49b56fe91cfc4100c8becd78
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-08-14 22:50:51 +0300

    feat(parcours): add service (create/update/get/list/activate/deactivate)

--- src/main/java/com/heigraduate/app/graduate/repository/ParcoursRepository.java
+++ src/main/java/com/heigraduate/app/graduate/repository/ParcoursRepository.java
@@ -1,9 +1,11 @@
 package com.heigraduate.app.graduate.repository;
 
-import com.heigraduate.app.model.Parcours;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import com.heigraduate.app.graduate.model.Parcours;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParcoursRepository extends JpaRepository<Parcours, UUID> {
