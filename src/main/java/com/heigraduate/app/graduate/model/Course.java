a1567b283252555b8e9f7d5774ec54367c9e8eb2
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-14 23:56:55 +0300

    feat(course): add Course JPA entity and update .gitignore

---
+++ src/main/java/com/heigraduate/app/graduate/model/Course.java
@@ -0,0 +1,30 @@
+package com.heigraduate.app.graduate.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.*;
+
+@Entity
+@Table(name = "course")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+
+  @Id @GeneratedValue private UUID id;
+
+  @Column(name = "course_reference", unique = true, nullable = false)
+  private String courseReference;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(name = "credits", nullable = false)
+  private Integer Credits;
+
+  @Column(nullable = false)
+  @Builder.Default
+  private Boolean active = true;
+}
