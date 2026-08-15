d07dad536e516ca3186337c9e1a0e6e9ab2572c1
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-08-16 00:49:40 +0300

    fix(security): replace Argon2PasswordEncoder with BCrypt to avoid NoClassDefFoundError on bouncycastle in lambda

--- src/main/java/com/heigraduate/app/security/SecurityConf.java
+++ src/main/java/com/heigraduate/app/security/SecurityConf.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,7 +30,7 @@ public class SecurityConf {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
