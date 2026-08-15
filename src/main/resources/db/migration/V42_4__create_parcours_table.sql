692e6b109eb5def3ef4511aa6f807fa5079e13b9
Christian RAVELOJAONA <hei.christian.3@gmail.com>
2026-08-15 20:52:45 +0300

    fix(preprod) : error migration
    * fix(db) : add if not exist in user table
    
    * fix(db) : remove db migration

--- src/main/resources/db/migration/V42_4__create_parcours_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS parcours (
-    id      UUID PRIMARY KEY,
-    code    VARCHAR(10) NOT NULL,
-    label   VARCHAR(255) NOT NULL,
-    active  BOOLEAN NOT NULL DEFAULT TRUE,
-    CONSTRAINT uk_parcours_code UNIQUE (code)
-);
