9c1a62b8e56197ad59cfd7814b734884fe1c3036
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-16 02:13:01 +0300

    chore(exception): remove unused CourseNotFoundException and DuplicateCourseReferenceException

--- src/main/java/com/heigraduate/app/graduate/exception/DuplicateCourseReferenceException.java
+++
@@ -1,8 +0,0 @@
-package com.heigraduate.app.graduate.exception;
-
-public class DuplicateCourseReferenceException extends RuntimeException {
-
-  public DuplicateCourseReferenceException(String courseReference) {
-    super("A course with reference '" + courseReference + "' already exists");
-  }
-}
