d776f40fdac5f40a13c22a7fe4cb9491dd1385df
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-08-16 02:05:08 +0300

    feat(mapper) : add student, teacher, academicYears, enrollement mapper

---
+++ src/main/java/com/heigraduate/app/graduate/model/StudentGroup.java
@@ -0,0 +1,24 @@
+package com.heigraduate.app.graduate.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.*;
+
+@Entity
+@Table(name = "student_group")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class StudentGroup {
+
+    @Id @GeneratedValue private UUID id;
+
+    @Column(nullable = false, length = 20)
+    private String reference; // e.g. "K1", "K2", "K3"
+
+    @Column(name = "max_size")
+    private Integer maxSize;
+}
