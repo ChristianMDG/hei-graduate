84209fd826e1b849d2bfa6821e616e6a21c0a98d
Christian RAVELOJAONA <hei.christian.3@gmail.com>
2026-08-16 13:57:13 +0300

    Revert "fix(db) : add migration table student, teacher, academic_year,enrolle…"
    This reverts commit 7d3aa501b31093b24fefd63de952db2b6e959eda.

--- src/main/resources/db/migration/V42_9__create_academic_year_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS academic_year (
-    id          UUID PRIMARY KEY,
-    label       VARCHAR(20) NOT NULL,
-    start_date  DATE NOT NULL,
-    end_date    DATE NOT NULL,
-    level       VARCHAR(10) NOT NULL
-);
