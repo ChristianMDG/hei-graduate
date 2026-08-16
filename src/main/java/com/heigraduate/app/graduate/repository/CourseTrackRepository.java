fb1663c4e8efe8d6d357e909e3c227c60a95295a
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-16 03:44:54 +0300

    feat(course-track): add CourseTrackRepository

---
+++ src/main/java/com/heigraduate/app/graduate/repository/CourseTrackRepository.java
@@ -0,0 +1,17 @@
+package com.heigraduate.app.graduate.repository;
+
+import com.heigraduate.app.graduate.model.CourseTrack;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseTrackRepository extends JpaRepository<CourseTrack, UUID> {
+
+    List<CourseTrack> findByTrackIdAndAcademicYearId(UUID trackId, UUID academicYearId);
+
+    List<CourseTrack> findByTrackIdAndAcademicYearIdAndObligatoryTrue(
+            UUID trackId, UUID academicYearId);
+
+    boolean existsByCourseIdAndTrackIdAndAcademicYearId(
+            UUID courseId, UUID trackId, UUID academicYearId);
+}
