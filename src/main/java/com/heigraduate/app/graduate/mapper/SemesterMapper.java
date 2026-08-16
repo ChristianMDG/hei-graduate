6c09c07708e6da2e7b670f1bfa69106af0d74c7b
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-16 03:16:46 +0300

    feat(semestre): add SemesterMapper

---
+++ src/main/java/com/heigraduate/app/graduate/mapper/SemesterMapper.java
@@ -0,0 +1,19 @@
+package com.heigraduate.app.graduate.mapper;
+
+import com.heigraduate.app.graduate.dto.SemesterResponse;
+import com.heigraduate.app.graduate.model.Semester;
+
+public class SemesterMapper {
+
+  private SemesterMapper() {}
+
+  public static SemesterResponse toResponse(Semester semester) {
+    return new SemesterResponse(
+        semester.getId(),
+        semester.getLabel(),
+        semester.getStartDate(),
+        semester.getEndDate(),
+        semester.getExpectedCredits(),
+        semester.getActive());
+  }
+}
