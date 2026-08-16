881b314c133c0813513b98ef40950046e1ba937e
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-16 03:49:52 +0300

    refactor(course-track): feat add CourseTrackMapper

---
+++ src/main/java/com/heigraduate/app/graduate/mapper/CourseTrackMapper.java
@@ -0,0 +1,21 @@
+package com.heigraduate.app.graduate.mapper;
+
+import com.heigraduate.app.graduate.dto.CourseTrackResponse;
+import com.heigraduate.app.graduate.model.CourseTrack;
+
+public class CourseTrackMapper {
+
+  private CourseTrackMapper() {}
+
+  public static CourseTrackResponse toResponse(CourseTrack ct) {
+    return new CourseTrackResponse(
+        ct.getId(),
+        ct.getCourse().getId(),
+        ct.getCourse().getCourseReference(),
+        ct.getTrack().getId(),
+        ct.getTrack().getCode(),
+        ct.getAcademicYear().getId(),
+        ct.getAcademicYear().getLabel(),
+        ct.getObligatory());
+  }
+}
