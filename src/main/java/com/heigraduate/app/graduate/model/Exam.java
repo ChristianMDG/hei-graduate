abf0d53d322d2e546f15dc09532c0d86feeaa2be
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-17 22:02:20 +0300

    fix(exam): add missing date, startTime, endTime fields

--- src/main/java/com/heigraduate/app/graduate/model/Exam.java
+++ src/main/java/com/heigraduate/app/graduate/model/Exam.java
@@ -2,6 +2,8 @@ package com.heigraduate.app.graduate.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.UUID;
 import lombok.*;
 
@@ -27,6 +29,15 @@ public class Exam {
 
   @Column(nullable = false)
   private String label;
+
+  @Column(nullable = false)
+  private LocalDate date;
+
+  @Column(name = "start_time", nullable = false)
+  private LocalTime startTime;
+
+  @Column(name = "end_time", nullable = false)
+  private LocalTime endTime;
 
   @Column(name = "coefficient", nullable = false, precision = 4, scale = 3)
   private BigDecimal coefficient;
