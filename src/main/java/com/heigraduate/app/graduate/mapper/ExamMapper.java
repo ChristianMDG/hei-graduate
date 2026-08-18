feac27514f859e8ab9f981d8d523e3c12ea621a8
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-18 11:21:32 +0300

    refactor(exam): map coefficientNumerator/coefficientDenominator in ExamMapper

--- src/main/java/com/heigraduate/app/graduate/mapper/ExamMapper.java
+++ src/main/java/com/heigraduate/app/graduate/mapper/ExamMapper.java
@@ -18,6 +18,7 @@ public class ExamMapper {
         exam.getDate(),
         exam.getStartTime(),
         exam.getEndTime(),
-        exam.getCoefficient());
+        exam.getCoefficientNumerator(),
+        exam.getCoefficientDenominator());
   }
 }
