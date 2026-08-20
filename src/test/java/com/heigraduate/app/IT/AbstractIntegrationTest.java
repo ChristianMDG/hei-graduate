01e80266b8e0cd760e2fda6e597293bd90dd6b33
Lahatranomena <hei.lahatra.3@gmail.com>
2026-08-20 08:11:45 +0300

    test(it): enable MockMvc on AbstractIntegrationTest

--- src/test/java/com/heigraduate/app/IT/AbstractIntegrationTest.java
+++ src/test/java/com/heigraduate/app/IT/AbstractIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.heigraduate.app.IT;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -9,6 +10,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @Testcontainers
 public abstract class AbstractIntegrationTest {
 
